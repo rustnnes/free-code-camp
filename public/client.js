ecd563905e97641fc3a26b75a6ce48eae1b1976c
Diego Rustnnes Bandeira <rustnnes@gmail.com>
2021-09-06 01:06:14 +0000

    feat(21)[OK]: Announce New Users

--- public/client.js
+++ public/client.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
   /*global io*/
   let socket = io();
 
-  socket.on('user count', function(data) {
-    console.log(data);
-  });
+  // socket.on('user count', function(data) {
+  //   console.log(data);
+  // });
 
+  socket.on('user', (data) => {
+    $('#num-users').text(data.currentUsers + ' users online');
+    let message = data.name + (data.connected ? ' has joined the chat.' : ' has left the chat.');
+    $('#messages').append($('<li>').html('<b>' + message + '</b>'));
+  });
+  
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
--- server.js
+++ server.js
@@ -65,13 +65,24 @@ myDB(async client => {
 
   io.on('connection', socket => {
     ++currentUsers;
-    io.emit('user count', currentUsers);
+    // io.emit('user count', currentUsers);
+
+    io.emit('user', {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true
+    });
     console.log('user ' + socket.request.user.name + ' connected');
 
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
       --currentUsers;
-      io.emit('user count', currentUsers);
+      io.emit('user', {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false
+      });
+      // io.emit('user count', currentUsers);
     });
   });
 
