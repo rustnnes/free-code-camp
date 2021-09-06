82776d199b7aa8760b737db8e32e4bf089e7f3d1
Diego Rustnnes Bandeira <rustnnes@gmail.com>
2021-09-06 00:56:45 +0000

    feat(20)[OK]: Authentication with Socket.IO

--- server.js
+++ server.js
@@ -6,16 +6,24 @@ const express = require('express');
 
 const session = require('express-session');
 const passport = require('passport');
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
+
+const auth = require('./auth');
 const myDB = require('./connection');
 const routes = require('./routes');
-const auth = require('./auth');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
 fccTesting(app); //For FCC testing purposes
+
 app.set('view engine', 'pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
@@ -25,11 +33,24 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 // const title = "Diego Bandeira — FCC Advanced Node and Express";
 
@@ -45,7 +66,7 @@ myDB(async client => {
   io.on('connection', socket => {
     ++currentUsers;
     io.emit('user count', currentUsers);
-    console.log('A user has connected');
+    console.log('user ' + socket.request.user.name + ' connected');
 
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
@@ -60,6 +81,18 @@ myDB(async client => {
       { title: e, message: 'Unable to login' });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
